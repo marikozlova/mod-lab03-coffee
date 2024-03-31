a797da99db3983b38aae1e4d1f3ec1323747d9ae
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-03-31 22:18:46 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,3 +1,4 @@
+// "Copyright [year] <Copyright Owner>"
 #ifndef INCLUDE_AUTOMATA_H_
 #define INCLUDE_AUTOMATA_H_
 #include <map>
@@ -5,26 +6,25 @@
 
 enum states {Off, Wait, Accept, Check, Cook};
 
-class Automata {
-private:
-    int cash;
-    std::string menu[3];
-    int prices[3];
-    states state;
-
-public:
-    Automata();
-    ~Automata();
-    void on();
-    void off();
-    void coin(int money);
-    void etMenu();
-    states getState();
-    void choice(int number);
-    bool check(int number);
-    void cancel();
-    void cook(int number);
-    void finish(int number);
+class Automata{
+    private:
+        int cash;
+        std::string menu[3];
+        int prices[3];
+        states state;
+    public:
+        Automata();
+        ~Automata();
+        void on();
+        void off();
+        void coin(int money);
+        void etMenu();
+        states getState();
+        void choice(int number);
+        bool check(int number);
+        void cancel();
+        void cook(int number);
+        void finish(int number);
 };
 #endif  // INCLUDE_AUTOMATA_H_
 
