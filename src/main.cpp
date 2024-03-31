5978d3aad643b878b65bfe350ecbd868a5769286
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-03-31 12:17:49 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Automata.h"
+#include <iostream>
+
+int main() {
+    Automata Machine;
+    Machine.on();
+    Machine.etMenu();
+    Machine.coin(200);
+    Machine.choice(2);
+    Machine.off();
+}
